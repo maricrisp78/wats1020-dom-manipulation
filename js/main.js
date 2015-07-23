eea0940f82474c1fa356978aa00cdeba790c915b
marieashley_78 <marieashley_78@hotmail.com>
2015-07-22 21:45:46 -0400

    commit jquery

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 //////////////////////////////////////////////////
 
 $( document ).ready(function() {
+	
+	
+										  
+	
     var userInfo = {
         firstName: 'Jane',
         lastName: 'Doe'
@@ -13,6 +17,8 @@ $( document ).ready(function() {
         greatest: 0,
         total: 0
     };
+	
+	
     // Place all your Javascript code inside this "document ready" function so
     // it does not run until the DOM is ready for Javascript manipulation.
 
@@ -22,18 +28,52 @@ $( document ).ready(function() {
     //      2. Fill the user's first and last name into `div.user-info`.
     //      (NOTE: You do not have to perform any validation on the data as
     //          a base requirement.)
+	
+	$('#login-form a').on('click', function() {
+		$('#login-form').hide();
+		$('.user-info').show();
+		$('.user-fullname').text(userInfo.firstName + ' ' + userInfo.lastName);
 
+	$('.btn-sm').on('click', function() {
+		$('.btn-sm').show();
+		$('#login-form').show();
+		$('.user-info').hide() ++;
+		
+	});
+	});		
+		
 
+//	$('.btn btn-default btn-sm').on('click', function();
+//		$('.btn btn-default btn-sm').toggle();
+//		});
+	
     // TODO: Create a function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
     // the content contained in the elements with the class "details" in the
     // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
+    //      1. When user clicks a "view details" button, find the parent of that element. √
     //      2. Within that parent, find all the elements that have the class `details`.
     //      3. Toggle visibility of all the elements within that parent with the class `details`.
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
 
+	$('.view-details').on('click', function(event){
+    console.log(event);
+	
+    var targetElement = event.target;
+    var container = targetElement.parentElement.parentElement;
+    $(container).find('.details').each(function(index, el){
+    if ($(el).is(':visible')){
+        $(el).fadeOut();
+        targetElement.innerText = "View Details";
+
+	} else {
+    $(el).fadeIn();
+    targetElement.innerText = "Hide Details";
+		
+		}
+    });
+    });
     // TODO: Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
     // then determines the updated vote breakdown to adjust the progress bars.
@@ -44,4 +84,46 @@ $( document ).ready(function() {
     //      4. Determine the respective percentages (out of 100) for each progress bar.
     //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
 
+$('.vote').on('click', function(){
+var userVote =$(this).data("vote");
+	if (userVote=="great") {
+	voteCounts.great++;
+	voteCounts.total++;
+		
+}
+	
+	if (userVote=="greatest") {
+		voteCounts.greatest++;
+		voteCounts.total++;
+	}
+
+var greatpercent=(voteCounts.great/voteCounts.total)* 100;
+
+var greatestpercent=(voteCounts.greatest/voteCounts.total)* 100;	
+	
+$(".great-progress").attr("style", "width: " + greatpercent + "%");
+$(".greatest-progress").attr("style", "width: " + greatestpercent + "%");	
+	
 });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
